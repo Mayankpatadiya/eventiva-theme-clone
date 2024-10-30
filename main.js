15a1f5a476697afead1a735e55dd46d0b45e3079
Mayank Patadiya <mayankpatadiya@28gmail.com>
2024-10-30 18:04:34 +0530

    eventiva theme

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+// window.addEventListener('scroll', () => {
+//     const main = document.querySelector('.image_aural1');
+//     const windowHeight = window.innerHeight;
+//     const reveal = main.getBoundingClientRect().top;
+//     const revealPoint = 200;
+
+//     if (reveal < windowHeight - revealPoint) {
+//         main.classList.add('leftToRight');
+//     } else {
+//         main.classList.remove('leftToRight');
+//     }
+// });
+
+
+// window.addEventListener('scroll', RightToLeft=()=>{
+
+//     var main =document.querySelector('.image_aural3');
+//     var windowHeight =window.innerHeight;
+//     var reveal = main.getBoundingClientRect().top;
+//     var revealPoint = 200;
+//     if(reveal < windowHeight - revealPoint)
+//     {
+//         main.classList.add('rightToLeft')
+//     }
+//     else{
+//         main.classList.remove('rightToLeft')
+
+//     }
+    
+// })
